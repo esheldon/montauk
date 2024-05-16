9640f39b92720b23aee23359c534ffb8d4d67955
Erin Sheldon <erin.sheldon@gmail.com>
2024-05-16 14:48:19 -0400

    test sky

--- mimsim/sky.py
+++ mimsim/sky.py
@@ -1,3 +1,85 @@
+def make_sky_image(
+    sky_model,
+    wcs,
+    nx,
+    ny,
+    gradient=True,
+    vignetting=True,
+    fringing=None,
+    sensor=None,
+    logger=None,
+):
+    """
+    Convenience function to make a sky image.  This is useful to ensure we get
+    the ordering of operations right
+
+    Parameters
+    ----------
+    sky_model: imsim.sky_model.SkyModel
+        The imsim model for the sky
+    wcs: galsim.GSFitsWCS
+        The WCS for the image
+    nx: int
+        Image size in x
+    ny: int
+        Image size in y
+    gradient: gradient object, optional
+        Must have an apply(image) method
+    vignetting: vignetting object, optional
+        Must have an apply(image) method
+    fringing: fringing object, optional
+        Must have an apply(image) method
+    sensor: galsim.SiliconSensor, optional
+        The sensor to use for modifying pixel areas
+    logger: python logger, optional
+        logger
+
+    Returns
+    -------
+    galsim.ImageF
+    """
+    import galsim
+
+    # constant so we can use value at the center
+    x = nx / 2
+    y = ny / 2
+    image_pos = galsim.PositionD(x=x, y=y)
+    sky_pos = wcs.toWorld(image_pos)
+    sky_level = sky_model.get_sky_level(sky_pos)
+
+    # this can be variable due to small pixel size variations
+    # due to non uniform WCS
+    sky_image = galsim.ImageF(nx, ny, wcs=wcs)
+
+    # values will vary due to wcs pixel size variations
+    wcs.makeSkyImage(sky_image, sky_level)
+
+    if gradient is not None:
+        if logger is not None:
+            logger.info('applying sky gradient')
+        gradient.apply(sky_image)
+
+    # should come after sky gradient
+    if vignetting is not None:
+        if logger is not None:
+            logger.info('applying sky vignetting')
+        vignetting.apply(sky_image)
+
+    if fringing is not None:
+        if logger is not None:
+            logger.info('applying fringing')
+        fringing.apply(sky_image)
+
+    if sensor is not None:
+        # TODO this uses orig_center of (0, 0), make sure correct
+        if logger is not None:
+            logger.info('applying pixel areas')
+        area = sensor.calculate_pixel_areas(sky_image)
+        sky_image *= area
+
+    return sky_image
+
+
 class FixedSkyGradient(object):
     """
     A class to apply a sky gradient
--- tests/test_sky.py
+++ tests/test_sky.py
@@ -3,6 +3,124 @@ import imsim
 import mimsim
 import mimsim.simtools
 import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize(
+    'options',
+    [{'gradient': False,
+      'fringing': False,
+      'vignetting': False,
+      'sensor_areas': False},
+     {'gradient': True,
+      'fringing': False,
+      'vignetting': False,
+      'sensor_areas': False},
+     {'gradient': False,
+      'fringing': True,
+      'vignetting': False,
+      'sensor_areas': False},
+     {'gradient': False,
+      'fringing': False,
+      'vignetting': True,
+      'sensor_areas': False},
+     {'gradient': False,
+      'fringing': False,
+      'vignetting': False,
+      'sensor_areas': True},
+     {'gradient': True,
+      'fringing': True,
+      'vignetting': True,
+      'sensor_areas': True}]
+)
+def test_make_sky(options):
+    # 88 is E2V
+    band = 'Y'
+    detnum = 88
+
+    seed = 31415
+    gs_rng = galsim.BaseDeviate(seed)
+
+    dm_detector = mimsim.camera.make_dm_detector(detnum)
+    obsdata = mimsim.simtools.load_example_obsdata()
+
+    wcs, _ = mimsim.wcs.make_batoid_wcs(
+        obsdata=obsdata, dm_detector=dm_detector,
+    )
+
+    sky_model = imsim.sky_model.SkyModel(
+        exptime=obsdata['exptime'],
+        mjd=obsdata['mjd'],
+        bandpass=obsdata['bandpass'],
+    )
+
+    if options['gradient']:
+        gradient = mimsim.sky.FixedSkyGradient(sky_model)
+    else:
+        gradient = None
+
+    if options['vignetting']:
+        vignetter = mimsim.vignetting.Vignetter(dm_detector)
+    else:
+        vignetter = None
+
+    if options['fringing']:
+        assert mimsim.fringing.should_apply_fringing(
+            band=band, dm_detector=dm_detector,
+        )
+        fringer = mimsim.fringing.Fringer(
+            boresight=obsdata['boresight'], dm_detector=dm_detector,
+        )
+    else:
+        fringer = None
+
+    if options['sensor_areas']:
+        sensor = mimsim.sensor.make_sensor(
+            dm_detector=dm_detector, gs_rng=gs_rng,
+        )
+    else:
+        sensor = None
+
+    bbox = dm_detector.getBBox()
+    nx = bbox.width
+    ny = bbox.height
+    image = mimsim.sky.make_sky_image(
+        sky_model=sky_model,
+        wcs=wcs,
+        nx=nx,
+        ny=ny,
+        gradient=gradient,
+        vignetting=vignetter,
+        fringing=fringer,
+        sensor=sensor,
+    )
+
+    test_image = image.copy()
+    test_image.array[:, :] = 1
+
+    x = nx / 2
+    y = ny / 2
+    image_pos = galsim.PositionD(x=x, y=y)
+    sky_pos = wcs.toWorld(image_pos)
+    sky_level = sky_model.get_sky_level(sky_pos)
+
+    # values will vary due to wcs pixel size variations
+    wcs.makeSkyImage(test_image, sky_level)
+
+    if options['gradient']:
+        gradient.apply(test_image)
+
+    if options['vignetting']:
+        vignetter.apply(test_image)
+
+    if options['fringing']:
+        fringer.apply(test_image)
+
+    if options['sensor_areas']:
+        area = sensor.calculate_pixel_areas(test_image)
+        test_image *= area
+
+    assert np.all(test_image.array[:, :] == image.array[:, :])
 
 
 def test_gradient():
