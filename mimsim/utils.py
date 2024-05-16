876826efab2211b3d12d5216e592633c362e862c
Erin Sheldon <erin.sheldon@gmail.com>
2024-05-16 14:53:22 -0400

    add get_latitude

--- mimsim/utils.py
+++ mimsim/utils.py
@@ -7,3 +7,13 @@ def extract_boresight(obsdata):
         obsdata['rightascension'] * galsim.degrees,
         obsdata['declination'] * galsim.degrees,
     )
+
+
+def get_latitude():
+    """
+    Get the latitude of the site as a galsim.Angle
+    """
+    import galsim
+    from lsst.obs.lsst.translators.lsst import SIMONYI_LOCATION as RUBIN_LOC
+
+    return RUBIN_LOC.lat.deg * galsim.degrees
