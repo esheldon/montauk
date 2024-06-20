458bfe724a57049fb88faf39481e2ac8c927ff1c
Erin Sheldon <erin.sheldon@gmail.com>
2024-06-20 12:25:14 -0400

    rename in example

--- examples/example-full.py
+++ examples/example-full.py
@@ -1,11 +1,11 @@
 import galsim
 import imsim
-import mimsim
+import montauk
 import numpy as np
 
 
 def main():
-    mimsim.logging.setup_logging('info')
+    montauk.logging.setup_logging('info')
 
     seed = 919
     rng = np.random.default_rng(seed)
@@ -16,19 +16,19 @@ def main():
 
     # default is 800, use 100 for speed
     psf_config = {'screen_size': 100}
-    cosmic_ray_rate = mimsim.defaults.DEFAULT_COSMIC_RAY_RATE
+    cosmic_ray_rate = montauk.defaults.DEFAULT_COSMIC_RAY_RATE
 
-    dm_detector = mimsim.camera.make_dm_detector(detnum)
+    dm_detector = montauk.camera.make_dm_detector(detnum)
 
     # load the metadata for some example data
-    obsdata = mimsim.simtools.load_example_obsdata(band=band)
+    obsdata = montauk.simtools.load_example_obsdata(band=band)
 
     # load the objects from the example data
-    cat = mimsim.simtools.load_example_instcat(
+    cat = montauk.simtools.load_example_instcat(
         rng=rng, band=band, detnum=detnum,
     )
 
-    wcs, icrf_to_field = mimsim.wcs.make_batoid_wcs(
+    wcs, icrf_to_field = montauk.wcs.make_batoid_wcs(
         obsdata=obsdata, dm_detector=dm_detector,
     )
 
@@ -38,32 +38,32 @@ def main():
         bandpass=obsdata['bandpass'],
     )
 
-    gradient = mimsim.sky.FixedSkyGradient(sky_model)
-    vignetter = mimsim.vignetting.Vignetter(dm_detector)
+    gradient = montauk.sky.FixedSkyGradient(sky_model)
+    vignetter = montauk.vignetting.Vignetter(dm_detector)
 
     # this only applies to E2V sensors in Y band
-    if mimsim.fringing.should_apply_fringing(
+    if montauk.fringing.should_apply_fringing(
         band=obsdata['band'], dm_detector=dm_detector,
     ):
-        fringer = mimsim.fringing.Fringer(
+        fringer = montauk.fringing.Fringer(
             boresight=obsdata['boresight'], dm_detector=dm_detector,
         )
     else:
         fringer = None
 
-    tree_rings = mimsim.tree_rings.make_tree_rings([detnum])
-    sensor = mimsim.sensor.make_sensor(
+    tree_rings = montauk.tree_rings.make_tree_rings([detnum])
+    sensor = montauk.sensor.make_sensor(
         dm_detector=dm_detector,
         tree_rings=tree_rings,
         gs_rng=gs_rng,
     )
 
-    dcr = mimsim.dcr.DCRMaker(
+    dcr = montauk.dcr.DCRMaker(
         bandpass=obsdata['bandpass'],
         hour_angle=obsdata['HA'],
     )
 
-    optics = mimsim.optics.OpticsMaker(
+    optics = montauk.optics.OpticsMaker(
         altitude=obsdata['altitude'],
         azimuth=obsdata['azimuth'],
         boresight=obsdata['boresight'],
@@ -74,7 +74,7 @@ def main():
         icrf_to_field=icrf_to_field,
     )
 
-    photon_ops_maker = mimsim.photon_ops.PhotonOpsMaker(
+    photon_ops_maker = montauk.photon_ops.PhotonOpsMaker(
         exptime=obsdata['vistime'],
         band=obsdata['band'],
         dcr=dcr,
@@ -88,7 +88,7 @@ def main():
         rotTelPos=obsdata['rotTelPos'],
     )
 
-    artist = mimsim.artist.Artist(
+    artist = montauk.artist.Artist(
         bandpass=obsdata['bandpass'],
         sensor=sensor,
         photon_ops_maker=photon_ops_maker,
@@ -96,19 +96,19 @@ def main():
         gs_rng=gs_rng,
     )
 
-    psf = mimsim.psfws.make_psfws_psf(
+    psf = montauk.psfws.make_psfws_psf(
         obsdata=obsdata,
         gs_rng=gs_rng,
         psf_config=psf_config,
     )
 
-    cosmics = mimsim.cosmic_rays.CosmicRays(
+    cosmics = montauk.cosmic_rays.CosmicRays(
         cosmic_ray_rate=cosmic_ray_rate,
         exptime=obsdata['vistime'],
         gs_rng=gs_rng,
     )
 
-    image, sky_image, truth = mimsim.runner.run_sim(
+    image, sky_image, truth = montauk.runner.run_sim(
         rng=rng,
         cat=cat,
         obsdata=obsdata,
