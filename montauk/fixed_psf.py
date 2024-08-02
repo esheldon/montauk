0b76cc2357a7e99dd4fe45c3676c9f9c52164831
Erin Sheldon <erin.sheldon@gmail.com>
2024-08-02 11:17:35 -0400

    support fixed psf

--- montauk/fixed_psf.py
+++ montauk/fixed_psf.py
@@ -1,12 +1,25 @@
 def make_fixed_psf(type, options):
     """
-    TODO add more types
+    make a fixed psf as a GSObject
+
+    Parameters
+    ----------
+    type: str
+        e.g. 'gauss' 'moffat'
+    options: dict
+        Options for the construction of the PSF
+
+    Returns
+    -------
+    GSObject
     """
     import galsim
+
     if type == 'gauss':
         psf = galsim.Gaussian(**options)
     elif type == 'moffat':
         psf = galsim.Moffat(**options)
     else:
-        raise ValueError(f'unsupported psf type: {psf_type}')
+        raise ValueError(f'unsupported psf type: {type}')
+
     return psf
--- tests/test_psf.py
+++ tests/test_psf.py
@@ -27,3 +27,42 @@ def test_eval_psf(psf_type):
 
     psf_at_pos = montauk.psf.eval_psf(psf, image_pos)
     assert psf_at_pos == expected_psf_at_pos
+
+
+@pytest.mark.parametrize('psf_type', ['psfws', 'gauss', 'moffat'])
+def test_fixed_psf(psf_type):
+
+    seed = 99
+
+    # default is 800, use 100 for speed
+    if psf_type == 'psfws':
+        psf_options = {'screen_size': 100}
+    else:
+        psf_options = {'fwhm': 1.0}
+        if psf_type == 'moffat':
+            psf_options['beta'] = 3.0
+
+    obsdata = montauk.simtools.load_example_obsdata()
+    image_pos = galsim.PositionD(25.2, 100.8)
+
+    if psf_type == 'psfws':
+        psf = montauk.psfws.make_psfws_psf(
+            obsdata=obsdata,
+            gs_rng=galsim.BaseDeviate(seed),
+            psf_config=psf_options,
+        )
+        expected_psf_at_pos = psf.getPSF(image_pos)
+    else:
+        psf = montauk.fixed_psf.make_fixed_psf(type=psf_type, options=psf_options)
+        expected_psf_at_pos = psf
+
+    psf_at_pos = montauk.psf.eval_psf(psf, image_pos)
+    assert psf_at_pos == expected_psf_at_pos
+
+    if psf_type != 'psfws':
+        if psf_type == 'gauss':
+            assert isinstance(psf, galsim.Gaussian)
+        elif psf_type == 'moffat':
+            assert isinstance(psf, galsim.Moffat)
+        else:
+            raise RuntimeError(f'unexpected psf type {psf_type}')
