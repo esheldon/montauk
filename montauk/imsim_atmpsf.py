47fd85cf5e6026f8851d021f0612a624eabf2a56
Erin Sheldon <erin.sheldon@gmail.com>
2024-09-17 11:49:45 -0400

    wrapper for imsim atm psf
    This is so we don't get a ChromaticConvolution object, simplifies
    the code later

--- montauk/imsim_atmpsf.py
+++ montauk/imsim_atmpsf.py
@@ -10,16 +10,16 @@ def make_imsim_atmpsf(obsdata, gs_rng, psf_config={}):
     gs_rng: galsim rng
         The galsim random number generator
     psf_config: dict, optional
-        Config for psf, can have t0, nlayers, screen_size, exponent,
-        kcrit, screen_scale, save_file, field_x, field_y
+        Config for psf, can have t0, kkcrit, screen_size, screen_scale,
+        exponent, nproc
 
     Returns
     --------
-    psfws.simulate_atm_psf.AtmosphericPSF
+    ImSimAtmosphericPSFWrapper
     """
     import imsim.atmPSF
 
-    return imsim.atmPSF.AtmosphericPSF(
+    atm_psf = imsim.atmPSF.AtmosphericPSF(
         airmass=obsdata['airmass'],
         rawSeeing=obsdata['rawSeeing'],
         band=obsdata['band'],
@@ -28,3 +28,33 @@ def make_imsim_atmpsf(obsdata, gs_rng, psf_config={}):
         exptime=obsdata['exptime'],
         **psf_config
     )
+    return ImSimAtmosphericPSFWrapper(atm_psf)
+
+
+class ImSimAtmosphericPSFWrapper:
+    """
+    Wrapper that always does second kick
+    """
+    def __init__(self, atm_psf):
+        self.atm_psf = atm_psf
+
+    def getPSF(self, field_pos, gsparams=None):  # noqa
+        import galsim
+
+        atm_psf = self.atm_psf
+
+        theta = (field_pos.x * galsim.arcsec, field_pos.y * galsim.arcsec)
+        return galsim.ChromaticAtmosphere(
+            atm_psf.atm.makePSF(
+                atm_psf.wlen_eff,
+                aper=atm_psf.aper,
+                theta=theta,
+                t0=atm_psf.t0,
+                exptime=atm_psf.exptime,
+                gsparams=gsparams,
+            ),
+            alpha=atm_psf.exponent,
+            base_wavelength=atm_psf.wlen_eff,
+            # Turns off DCR, since we apply that later using PhotonDCR.
+            zenith_angle=0 * galsim.degrees
+        )
